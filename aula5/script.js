f30bfa8c4173d8c24e070cbe987b7889b44dd9b6
marianaaayoub <marianaadel0301@gmail.com>
2026-02-24 11:44:00 -0300

    first commit

---
+++ aula5/script.js
@@ -0,0 +1,28 @@
+//funções em js 
+
+//uma fução é nada mais que um codigo reutilizavel, criado para executar um tarefa especifica 
+//function nomedafuncao (paremetro1,parametro2){
+    //codigo que sra executado
+//return resultado;
+//}
+
+//exemplo 01 - somar dois numeros
+//function somar (a,b){
+//return a + b;
+//}
+//console.log(somar(2,3));
+
+//function realparadolar(valorreal,cotação){
+ //   return valorreal/ cotação
+//}
+//console.log(realparadolar(20,5.17).toFixed(2))
+
+//calcular aumento de salario
+//function calcularamento(salario){
+//   return salario + (salario * 0.25)
+//}
+//console.log(calcularamento(10000))
+
+for (let i=1; i <=5; i++){
+    console.log()
+}
